a2300a935644e20b53976724ecea317c278d0116
Baile <Baile@Baile-pc>
2015-07-13 00:33:47 -0300

    sons atualizados

--- src/Enemy3.cpp
+++ src/Enemy3.cpp
@@ -8,7 +8,7 @@
 #include "Game.h"
 #include "State.h"
 
-Enemy3::Enemy3(float x, float y, GameObject* planet, float initialRotation, float alturaInicial) : sp("img/enemy3.png", 0.30, 3, 7), speed(), startPos(x, y), dmgCD(), knockback(), shootcd()
+Enemy3::Enemy3(float x, float y, GameObject* planet, float initialRotation, float alturaInicial) : sp("img/enemy3.png", 0.375, 3, 7), speed(), startPos(x, y), dmgCD(), knockback(), shootcd()
 {
 	this->planet = planet;
 	sp.SetScaleX(0.5);
@@ -82,6 +82,13 @@ void Enemy3::Update(float dt)
 		sp.Update(1);
 	}
 
+	if ((sp.GetCurrentFrame() == 0) || (sp.GetCurrentFrame() == 7))
+	{
+		Sound* sound = new Sound("audio/tiro3f.wav");
+		sound->Play(0);
+		delete(sound);
+	}
+
 	somaRotation = planet->somaRotation;
 	rotation += somaRotation;
 
@@ -153,7 +160,7 @@ void Enemy3::Shoot (Point pos)
 		}
 	}
 
-	Bullet* bullet = new Bullet(planet,rotation, 100, speed, 2000,"img/enemy3_bullet.png", true, 4);
+	Bullet* bullet = new Bullet(xBullet, yBullet, angle, 100, 2000,"img/enemy3_bullet.png", 4, true);
 	Game::GetInstance().GetCurrentState().AddObject(bullet);
 }
 
--- src/WeaponSword.cpp
+++ src/WeaponSword.cpp
@@ -58,7 +58,7 @@ void WeaponSword::Update(float dt)
 
 	if(InputManager::GetInstance().KeyPress(SDLK_a) || atkFrame)
 	{
-		Sound* sound = new Sound("audio/vassoura.wav");
+		Sound* sound = new Sound("audio/espada.wav");
 		sound->Play(0);
 		delete(sound);
 		atkFrame = true;
